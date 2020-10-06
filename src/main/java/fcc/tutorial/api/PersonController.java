042389d19b0bb826b997fb5e23676428c3953354
John Hattas <john.d.hattas@nielsen.com>
2020-10-05 23:56:33 -0500

    add delete and update person methods

--- src/main/java/fcc/tutorial/api/PersonController.java
+++ src/main/java/fcc/tutorial/api/PersonController.java
@@ -29,9 +29,19 @@ public class PersonController {
         return personService.getAllPeople();
     }
 
-    @GetMapping(path = "/{id}")
+    @GetMapping(path = "{id}")
     public Person getPersonById(@PathVariable("id") UUID id) {
         return personService.getPersonById(id)
                 .orElse(null);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deletePersonById(@PathVariable("id") UUID id) {
+        personService.deletePerson(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public void updatePerson(@PathVariable("id") UUID id, @RequestBody Person personToUpdate) {
+        personService.updatePerson(id, personToUpdate);
     }
 }
